3d81fb1590d7c1ee18f4e6f3a44e5c8dc0a7adf5
SKY-20160815JRX\Administrator <wangru0926@sina.com>
2016-12-04 23:24:19 +0800

    222

--- app/src/main/java/com/dspy/material_news/AppStartActivity.java
+++ app/src/main/java/com/dspy/material_news/AppStartActivity.java
@@ -24,10 +24,6 @@ public class AppStartActivity extends AppCompatActivity {
      * user interaction before hiding the system UI.
      */
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-	
-	
-	
-	
 
     /**
      * Some older devices needs a small delay between UI widget updates
