6f6984a37a388f7816ce58ae8fe64b849a279486
duxueyang <duxueyangfans@163.com>
2016-12-05 11:49:31 +0800

    提交diamante

--- app/src/main/java/com/dspy/material_news/AppStartActivity.java
+++ app/src/main/java/com/dspy/material_news/AppStartActivity.java
@@ -20,7 +20,7 @@ public class AppStartActivity extends AppCompatActivity {
     private static final boolean AUTO_HIDE = true;
 
 
-    
+
 
     /**
      * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
