6ddc67f671471a389b252de67e4df30dfe17be95
duxueyang <duxueyangfans@163.com>
2017-03-01 15:37:03 +0800

    重新整理代码

---
+++ app/src/main/java/com/mindimps/module/init/presenter/ADPresenter.java
@@ -0,0 +1,17 @@
+package com.mindimps.module.init.presenter;
+
+/**
+ * Created by duxueyang on 2017/3/1.
+ */
+
+public class ADPresenter  extends   ADContract.Presenter {
+    @Override
+    public void getADEntity() {
+
+    }
+
+    @Override
+    public void onAttached() {
+
+    }
+}
